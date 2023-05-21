9a387a2c13242d4985ccc9257654beab77ab1910
MauroCoone <105227656+MauroCoone@users.noreply.github.com>
2023-05-21 18:54:06 +0200

    project pagina

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 const navbar = document.querySelector('#navbar')
+const list = document.querySelector('.navlist')
+
 
 window.addEventListener('scroll', () => {
   if (window.scrollY > 20) {
     navbar.classList.add('scrolled')
+    list.classList.add('scrolled')
   } else {
     navbar.classList.remove('scrolled')
+    list.classList.remove('scrolled')
   }
 })
